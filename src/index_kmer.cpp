f586a08039d3e049050b1766395aaffcbc7fa72e
khodor14 <khodor.hannouche@gmail.com>
2022-11-15 15:48:14 +0100

    inserted procedures to modify the index of the graph

--- src/index_kmer.cpp
+++ src/index_kmer.cpp
@@ -6,26 +6,23 @@
 #include <string>
 #include <tuple>
 
-Index::Index(int buckets){
+Index::Index(int buckets,int k_size){
+    k=k_size;
     number_of_buckets=buckets;
     index_table.rehash(number_of_buckets);
 }
 
-void Index::create(GfaGraph& graph,int k){
+void Index::create(GfaGraph& graph){
     std::vector<Node> nodes=graph.get_nodes();
     auto iter_nodes=nodes.begin();
     while(iter_nodes!=nodes.end()){
         int id=iter_nodes->get_id();
         std::string unitig=iter_nodes->get_unitig();
-
-        for(int i=0;i<unitig.length()-k+2;i++){
-            std::string sub=unitig.substr(i,i+k-1);
-            std::string rc=getCanonical(sub);
-            std::string toStore=std::min(sub,rc);
-            bool orientation=true;
-            if(std::strcmp(sub.c_str(),rc.c_str())>0){
-                orientation=false;
-            }
+        int i=0;
+        for(int i=0;i<=unitig.length()-k+1;++i){
+            std::string sub=unitig.substr(i,k-1);
+            std::string toStore=getCanonical(sub);
+            bool orientation=isCanonical(sub);
             if(index_table.find(toStore)==index_table.end()){
                     std::vector<std::tuple<int,int,bool>> data;
                     data.push_back(std::tuple<int,int,bool>(id,i,orientation));
@@ -37,6 +34,7 @@ void Index::create(GfaGraph& graph,int k){
                 index_table[toStore]=data;
 
             }
+            i=i+1;
         }
         iter_nodes++;
 	}
@@ -48,41 +46,66 @@ std::vector<std::tuple<int,int,bool>> Index::find(std::string kmer){
     return index_table.at(getCanonical(kmer));
 }
 void Index::update_k_1_mer(std::string k_1_mer,int prev_id,int current_id,int position){
-    std::vector<std::tuple<int,int,bool>> data=index_table.at(getCanonical(k_1_mer));
+    /*
+        the input are: a string of size (k-1)
+        the previous id where this string occurs
+        the current id where it occurs after the updte performed
+        the new position where it occurs
+    */
+    std::vector<std::tuple<int,int,bool>> data=find(k_1_mer);//get the occurences of the string from the index
+
+    //go over the occurences
     for (std::tuple<int, int,bool>& tup : data){
+        //which occurens has the previous id, the old occurence
         if(std::get<0>(tup)==prev_id){
-            std::get<0>(tup)=current_id;
-            std::get<1>(tup)=position;
-            break;
+            std::get<0>(tup)=current_id;//update id
+            std::get<1>(tup)=position;//update positions
+            break;//break since the (k-1)-mer occurs once in a unitig except for palindromic k-mers
         }
     }
+    index_table[getCanonical(k_1_mer)]=data;
 }
-void Index::update_unitig(std::string seq,int id,int previous_id,int k){
+void Index::insert(std::string k_1_mer,int id,int position){
+    /*
+        the input is a string of size (k-1)
+        the id of the unitig where this string occurs
+        the position
 
-    for(int i=0;i<seq.length()-k+2;i++){
-        update_k_1_mer(seq.substr(i,i+k-1),previous_id,id,i);
+        The k-1-mer gets inserted into the index
+    */
+   std::vector<std::tuple<int,int,bool>> occurences=find(k_1_mer);//get previous occurences
+
+   bool orientation=isCanonical(k_1_mer);//find the orientation
+   occurences.push_back(std::tuple<int,int,bool>(id,position,orientation));//insert the new occurence into vector
+   index_table[getCanonical(k_1_mer)]=occurences;
+
+}
+void Index::insertSubUnitig(std::string unitig,int id,int starting_position,int ending_position){
+    /*
+        the inputs are:
+                     a string unitig
+                     its id
+                     the starting position (int)
+                     the ending position (int)
+        Actions performed: all the (k-1)-mers occuring between starting_position and ending position gets inserted
+    */
+
+   for(int position=starting_position;position<=ending_position;position++){
+        //call the function insert to insert the (k-1)-mer at position i
+        insert(unitig.substr(position,k-1),id,position);
+   }
+}
+void Index::update_unitig(std::string seq,int current_id,int previous_id,int starting_position,int ending_position){
+    /*
+        the inputs are:
+                     a string unitig
+                     its current id
+                     its previous id
+                     the starting position (int)
+                     the ending position (int)
+        Actions performed: the info all the (k-1)-mers occuring between starting_position and ending position get updated
+    */
+    for(int position=starting_position;position<=ending_position;position++){
+        update_k_1_mer(seq.substr(position,k-1),previous_id,current_id,position);
     }
-}
-void Index::add_unitig(std::string unitig,int id,int k){
-
-        for(int i=0;i<unitig.length()-k+2;i++){
-            std::string sub=unitig.substr(i,i+k-1);
-            std::string rc=getCanonical(sub);
-            std::string toStore=std::min(sub,rc);
-            bool orientation=true;
-            if(std::strcmp(sub.c_str(),rc.c_str())>0){
-                orientation=false;
-            }
-            if(index_table.find(toStore)==index_table.end()){
-                    std::vector<std::tuple<int,int,bool>> data;
-                    data.push_back(std::tuple<int,int,bool>(id,i,orientation));
-                    index_table[toStore]=data;
-            }
-            else{
-                std::vector<std::tuple<int,int,bool>> data=index_table.at(toStore);
-                data.push_back(std::tuple<int,int,bool>(id,i,orientation));
-                index_table[toStore]=data;
-
-            }
-        }
 }
--- src/index_kmer.h
+++ src/index_kmer.h
@@ -11,7 +11,7 @@ class Index
 private:
     /* data */
     int number_of_buckets;//the maximum number of buckets
-
+    int k;//the size of the k-mer
     /*
     we use a map<(k-1)-mer,a vector tuple<unitig id,position orientation>> to index all the unitigs
     using a tuple to store the index <unitig id,position,orientation> of each (k-1)-mer
@@ -21,10 +21,10 @@ private:
     
 public:
     //initialize the index
-    Index(int number_of_buckets);
+    Index(int number_of_buckets,int k);
 
     //create it from the unitigs of the graph
-    void create(GfaGraph & graph,int k);
+    void create(GfaGraph & graph);
 
     //this function returns the index of a (k-1)-mer, its all occurences which is equal to 8 at max
     std::vector<std::tuple<int,int,bool>> find(std::string k_1_mer);
@@ -32,11 +32,12 @@ public:
     //update the table for one (k-1)-mer i.e change the id and position of the unitig
     void update_k_1_mer(std::string k_1_mer,int prev_id,int current_id,int position);
 
+    //insert a (k-1)-mer in unitig u of id id at the given position into the index
+    void insert(std::string k_1_mer,int id,int position);
+    //insert all (k-1)-mers in unitig from starting till ending
+    void insertSubUnitig(std::string unitig,int id,int starting_position,int ending_position);
     //updating for all the (k-1)-mers of the unitig
-    void update_unitig(std::string seq,int id,int previous_id,int k);
-
-    //adding a new created unitig
-    void add_unitig(std::string unitig,int id,int k);
+    void update_unitig(std::string seq,int id,int previous_id,int starting_position,int ending_position);
     ~Index()=default;
 };
 #endif // !index_kmer_H
